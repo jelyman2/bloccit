0dd09bee32a64308392451c73315ce875cab190e
Johnathan Lyman <jelyman@gmail.com>
2015-12-02 23:42:58 -0800

    Assignment 33: Questions

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,6 +17,17 @@ posts = Post.all
   )
 end
 
+# Create Questions
+
+100.times do
+  Question.create!(
+    title: RandomData.random_question,
+    body:  RandomData.random_paragraph,
+    resolved: false
+  )
+end
+
 puts "Seed finished"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
+puts "#{Question.count} questions created"
