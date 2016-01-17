7b72ab49f7b7605fe612ccb9df532cb1173b4360
Johnathan Lyman <jelyman@gmail.com>
2016-01-17 11:29:35 -0800

    Checkpoint 39: Posts and Users

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,16 @@
 include RandomData
 
+# Create Users
+
+5.times do
+  User.create!(
+      name:     RandomData.random_name,
+      email:    RandomData.random_email,
+      password: RandomData.random_sentence
+  )
+end
+users = User.all
+
 # Create Topics
 15.times do
   Topic.create!(
@@ -12,9 +23,10 @@ topics = Topic.all
 # Create Posts
 50.times do
   Post.create!(
-    topic: topics.sample,
-    title:  RandomData.random_sentence,
-    body:   RandomData.random_paragraph
+      user:   users.sample,
+      topic:  topics.sample,
+      title:  RandomData.random_sentence,
+      body:   RandomData.random_paragraph
   )
 end
 posts = Post.all
@@ -22,12 +34,19 @@ posts = Post.all
 # Create Comments
 100.times do
   Comment.create!(
-    post:  posts.sample,
-    body: RandomData.random_paragraph
+      post:  posts.sample,
+      body: RandomData.random_paragraph
   )
 end
 
+user = User.first
+user.update_attributes!(
+    email: 'jelyman@gmail.com',
+    password: 'helloworld'
+)
+
 puts "Seed finished"
+puts "#{User.count} users created"
 puts "#{Topic.count} topics created"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
