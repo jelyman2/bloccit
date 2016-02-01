207466744f4afa67828d5c0ac06f3fda3d3b4cf8
Johnathan Lyman <jelyman@gmail.com>
2016-01-31 20:32:28 -0800

    Checkpoint 42: Labels

--- spec/models/topic_spec.rb
+++ spec/models/topic_spec.rb
@@ -4,6 +4,10 @@ include RandomData
 RSpec.describe Topic, type: :model do
   let(:topic) { Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph) }
 
+  it { is_expected.to have_many(:posts) }
+  it { is_expected.to have_many(:labelings) }
+  it { is_expected.to have_many(:labels).through(:labelings) }
+
   describe "attributes" do
     it "should respond to name" do
       expect(topic).to respond_to(:name)
