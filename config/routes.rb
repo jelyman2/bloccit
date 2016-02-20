1c2d702d18314ad28b297ab3a2c1b09fdb7f86b1
Johnathan Lyman <jelyman@gmail.com>
2016-02-20 13:08:24 -0800

    Checkpoint 45: Finished with fixed FG methods

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,6 @@ Rails.application.routes.draw do
 
   root to: 'welcome#index'
 
-  resources :users, only: [:new, :create]
+  resources :users, only: [:new, :create, :show]
   resources :sessions, only: [:new, :create, :destroy]
 end
