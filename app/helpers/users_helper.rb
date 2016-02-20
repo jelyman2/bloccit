479a3a2eca386bac364128f6eeef41508eb6d62f
Johnathan Lyman <jelyman@gmail.com>
2016-02-20 13:48:07 -0800

    Assignment 45 Part 3.01: Fixed display bug on user show page

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,9 +1,13 @@
 module UsersHelper
-  def user_created_posts?
-    true unless user.posts.count.nil?
+  def user_created_posts?(user)
+    user.posts.count.nil?
   end
 
-  def user_created_comments?
-    true unless user.comments.count.nil?
+  def user_created_comments?(user)
+    user.comments.count.nil?
+  end
+
+  def user_favorited_posts?(user)
+    user.favorites.count.nil?
   end
 end
